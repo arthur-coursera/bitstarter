64dccfc42a1ebddd7a1c9cc4a19794068e5ed306
Arthur Yip <arthur.coursera@gmail.com>
2013-07-10 17:12:53 +0000

    Empty

--- web.js
+++ web.js
@@ -6,10 +6,11 @@ var fs = require('fs');
 var content = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-    esponse.send(content);
+    response.send(content); 
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
